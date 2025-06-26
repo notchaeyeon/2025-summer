fca041c2372c244cbb2b4aece387974bb7ee7c09
notchaeyeon <163790294+notchaeyeon@users.noreply.github.com>
2025-06-26 13:46:28 +0900

    25th June, 2025

---
+++ DownCasting.java
@@ -0,0 +1,12 @@
+package ch02;
+
+public class DownCasting {
+
+	public static void main(String[] args) {
+		double tall = 176.6;
+		double weight = 74.34;
+		System.out.printf("신장: %d cm \n", (int) tall);
+		System.out.printf("몸무게: %d kg \n", (int) weight);
+		
+	}
+}
